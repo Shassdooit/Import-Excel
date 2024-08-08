bbf38297044c4497c5783c834f90447d8f6fa4a1
nicolaychudakov <nicolaychudakov@gmail.com>
2024-08-08 21:31:50 +0300

    fix script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
@@ -5,3 +7,12 @@ docker run --rm \
     laravelsail/php82-composer:latest \
     composer install --ignore-platform-reqs
 
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
+./vendor/bin/sail up -d
+./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan migrate
+./vendor/bin/sail npm install
+./vendor/bin/sail artisan storage:link
