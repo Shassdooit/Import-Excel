617d505e07322debbac84ba88b23fa86610d5449
nicolaychudakov <nicolaychudakov@gmail.com>
2024-08-08 14:02:53 +0300

    add resources & requests + update ProductImportController

--- app/Http/Controllers/ProductImportController.php
+++ app/Http/Controllers/ProductImportController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Product\StoreProductRequest;
+use App\Http\Requests\Product\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Imports\ProductImport;
 use App\Models\Product;;
-
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -28,11 +30,26 @@ class ProductImportController extends Controller
     public function showProducts(): View
     {
         $products = Product::paginate(9);
-        return view('products.index', compact('products'));
+        return view('products.index', ['products' => ProductResource::collection($products)]);
     }
 
     public function showProduct(Product $product): View
     {
-        return view('products.show', compact('product'));
+        $product->load('additionalFields', 'images');
+        return view('products.show', ['product' => new ProductResource($product)]);
+    }
+
+    public function store(StoreProductRequest $request): RedirectResponse
+    {
+        $product = Product::create($request->validated());
+
+        return redirect()->route('products.show', $product);
+    }
+
+    public function update(UpdateProductRequest $request, Product $product): RedirectResponse
+    {
+        $product->update($request->validated());
+
+        return redirect()->route('products.show', $product);
     }
 }
